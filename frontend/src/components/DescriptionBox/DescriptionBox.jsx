a1b5eda7028b448e109da5efd002c7bfc6c6e5b6
PrathyushaReddy-17 <102314564+PrathyushaReddy-17@users.noreply.github.com>
2024-02-25 15:34:22 +0530

    first commit

---
+++ frontend/src/components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An ecommerce platform is a service that allows you to make money online through your own website. an ecommerce company  enables individuals, creators, and businesses of all sizes to sell online and in person through a brick-and-mortar store.</p>
+      <p>
+      It’s the hub of information about a company and what they sell. On an eCommerce website, you’ll find product listings, eCommerce blog content, company history, and contact information.
+      </p>
+      
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
